4939b7e4e0ed3a693c8e551c07185c921666079d
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-03-18 11:21:52 -0700

    remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response=requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        body_responses_list =[]
+        body_responses= json.loads(self.get_response_body())
+        for body_response in body_responses:
+            body_responses_list.append(body_response)
+        return body_responses_list
